100a19ef4ad86577889c8ba7a4b36d2c527855f8
Danny Hyun <hyun.danny+github@gmail.com>
2014-12-05 18:59:15 -0500

    restricting to use 1 thread for EventLoopGroup and using blocking and promise

--- src/main/java/ratpack/example/java/HandlerFactory.java
+++ src/main/java/ratpack/example/java/HandlerFactory.java
@@ -19,6 +19,22 @@ public class HandlerFactory implements ratpack.launch.HandlerFactory {
 
                         .handler("bar", context -> context.render("from the bar handler")) // Map to /bar
 
+                        .handler("long1", context -> context.blocking(() -> {
+                            Thread.sleep(10000);
+                            return "long1";
+                        }).then(context::render))
+
+                        .handler("long2", context -> context.promise(f -> new Thread() {
+                            public void start() {
+                                try {
+                                    Thread.sleep(10000);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                f.success("long2");
+                            }
+                        }.start()).then(context::render))
+
                         .prefix("nested", nested -> { // Set up a nested routing block, which is delegated to `nestedHandler`
                             nested.handler(":var1/:var2?", context -> { // The path tokens are the :var1 and :var2 path components above
                                 Map<String, String> pathTokens = context.getPathTokens();
